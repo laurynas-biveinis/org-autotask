dc6c712be753de50ab772289e07d9a3a3c1fa70c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-11-15 06:57:27 +0200

    Fix error in the previous commit

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,4 +4,4 @@
                       . ((org-autotask-with-org-node-with-url . 1)
                          (org-autotask--test-fixture . 1)
                          (org-autotask--buffer-test . 1)
-                         (org-autotask--clock-in-action-test . 1))))))
+                         (org-autotask--clock-in-action-test . 2))))))
